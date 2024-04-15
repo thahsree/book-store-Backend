1964f55e146e0d77c59d4d6d6f6ba690f76043a6
thashreef <chthashreef22@gmail.com>
2024-04-15 20:14:16 +0530

    added user condition

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -32,44 +32,49 @@ function CreateBook(props) {
                 console.log(err);
             })
     }
+    const isUser = localStorage.getItem('authToken');
     return (
+        isUser ? 
         <div className='p-4'>
             <BackButton />
             <h1 className='text-3xl my-4'>Create Book</h1>
-            { loading ? <Spinner/> : ''}
+            {loading ? <Spinner /> : ''}
+            {error && <div className="text-red-500">{error}</div>}
             <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
                 <div className='my-4'>
                     <label className='text-xl mr-4 text-gray-500'>Title</label>
                     <input
-                     type="text" 
-                     value={title}
-                     onChange={(e)=> setTitle(e.target.value)}
-                     className='border-2 border-gray-500 px-4 py-2 w-full'
-                     />
+                        type="text" 
+                        value={title}
+                        onChange={(e)=> setTitle(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2 w-full'
+                    />
                 </div>
                 <div className='my-4'>
                     <label className='text-xl mr-4 text-gray-500'>Author</label>
                     <input
-                     type="text" 
-                     value={author}
-                     onChange={(e)=> setAuthor(e.target.value)}
-                     className='border-2 border-gray-500 px-4 py-2 w-full'
-                     />
+                        type="text" 
+                        value={author}
+                        onChange={(e)=> setAuthor(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2 w-full'
+                    />
                 </div>
                 <div className='my-4'>
                     <label className='text-xl mr-4 text-gray-500'>Published Year</label>
                     <input
-                     type="text" 
-                     value={publishedYear}
-                     onChange={(e)=> setPublishedYear(e.target.value)}
-                     className='border-2 border-gray-500 px-4 py-2 w-full'
-                     />
+                        type="text" 
+                        value={publishedYear}
+                        onChange={(e)=> setPublishedYear(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2 w-full'
+                    />
                 </div>
-                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+                <button className={`p-2 bg-sky-300 m-8 ${loading ? 'pointer-events-none opacity-50' : ''}`} onClick={handleSaveBook} disabled={loading}>
                     Save
                 </button>
             </div>
         </div>
+        :
+        navigate('/')
     );
 }
 
